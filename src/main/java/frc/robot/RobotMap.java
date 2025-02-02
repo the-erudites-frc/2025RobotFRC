688f2ecc7619860c533c0ef4169b4a8c617cde2b
the-erudites-frc <165996010+the-erudites-frc@users.noreply.github.com>
2025-02-02 17:12:47 -0500

    Adding Correct Drive Train Code

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public class RobotMap {
+
+    public static final int MOTOR_LEFT_1 = 0;
+    public static final int MOTOR_LEFT_2 = 1;
+    public static final int MOTOR_RIGHT_1 = 2;
+    public static final int MOTOR_RIGHT_2 = 3;
+    public static final int DRIVER_CONTROLLER = 0;
+    public static final int LEFT_STICK_Y = 0;
+    public static final int RIGHT_STICK_Y = 0;
+    
+}
