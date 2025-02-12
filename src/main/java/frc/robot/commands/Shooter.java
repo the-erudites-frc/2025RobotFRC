548c4a1f85411c85fde303b089c637f5135335f0
Adriang1224 <agegar7@gmail.com>
2025-02-11 19:17:44 -0500

    one of my things didn't save, so saving again

--- src/main/java/frc/robot/commands/Shooter.java
+++ src/main/java/frc/robot/commands/Shooter.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralShooter;
 
-public class Shooter {
+public class Shooter extends Command {
     
 }
